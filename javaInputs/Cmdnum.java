5be33c18c9d98c5c4ecefd3a2803f2145df4a924
Malini Sureshkumar <malinisureshkumar2022@gmail.com>
2025-07-04 23:09:15 +0530

    Add files via upload

---
+++ javaInputs/Cmdnum.java
@@ -0,0 +1,12 @@
+public class Cmdnum {
+    public static void main(String[] args){
+        //String into integer
+        int num1=Integer.valueOf(args[0]);
+        int num2=Integer.valueOf(args[1]);
+
+        System.out.println("The Sum of "+num1+" +"+num2+" is");
+        System.out.println(num1+num2);
+
+    }
+    
+}
---
+++ javaInputs/Inputs.java
@@ -0,0 +1,8 @@
+public class Inputs {
+    public static void main(String[] args) {
+       
+        String cmdstr1= args[0];
+        String cmdstr2=args[1];
+        System.out.println(cmdstr1+" Technologies "+cmdstr2);
+    }
+}
